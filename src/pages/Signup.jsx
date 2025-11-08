f3273c7de2131d5a4071b30c9b058dd9c0a04df3
SriVenkatG <2300031666@kluniversity.in>
2025-11-08 08:55:06 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -10,7 +10,8 @@ export default function Signup() {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8081/auth/signup", form);
+      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/signup`, form);
+      
       alert(res.data);
       navigate("/");
     } catch {
